cc0b89ffe3b1fae5f21bcd19105153330c431018
Brenden2008 <brendenstahle@gmail.com>
2022-03-31 19:41:19 +0000

    less goo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 const Deta = require('deta');
 
 const deta = Deta("project key");
@@ -23,7 +23,7 @@ const config = {
 // license_type
 
 app.post("/api/createlicense", async (req, res) => {
-    if (req.query.apikey == config.apikey){
+    if (req.query.apikey == config.apikey) {
         let myuuid = uuidv4();
         var license = {
             license_name: req.body.license_name,
@@ -32,14 +32,14 @@ app.post("/api/createlicense", async (req, res) => {
             license_expires: req.body.license_expires,
             license_type: req.body.license_type
         }
-        try{
+        try {
             await db.put(license, myuuid)
-            res.status(200).json({success: 1, license: license})
-        } catch (err){
-            res.status(500).json({success: 0, error: "License could not be created."})
+            res.status(200).json({ success: 1, license: license })
+        } catch (err) {
+            res.status(500).json({ success: 0, error: "License could not be created." })
         }
     } else {
-        res.status(401).json({success: 0, error: "APIKEY does not match on the server."})
+        res.status(401).json({ success: 0, error: "APIKEY does not match on the server." })
     }
 });
 
@@ -47,23 +47,19 @@ app.post("/api/createlicense", async (req, res) => {
 // apikey
 // license_id
 app.get("/api/getlicense", async (req, res) => {
-    if (req.query.apikey == config.apikey){
-        try{
-            var license = await db.get(req.query.license_id);
-            if(license != null){
-                if (license.license_expires == false){
-                    res.status(200).json({success: 1, license: license, license_isvalid: true})
-                } else if (license.license_expires < Date()) {
-                    res.status(500).json({success: 0, error: "License expired."})
-                } else {
-                    res.status(200).json({success: 1, license: license, license_isvalid: true})
-                }
+    try {
+        var license = await db.get(req.query.license_id);
+        if (license != null) {
+            if (license.license_expires == false) {
+                res.status(200).json({ success: 1, license: license, license_isvalid: true })
+            } else if (license.license_expires < Date()) {
+                res.status(500).json({ success: 0, error: "License expired." })
+            } else {
+                res.status(200).json({ success: 1, license: license, license_isvalid: true })
             }
-        } catch (err){
-            res.status(500).json({success: 0, error: "License could not be read."})
         }
-    } else {
-        res.status(401).json({success: 0, error: "APIKEY does not match on the server."})
+    } catch (err) {
+        res.status(500).json({ success: 0, error: "License could not be read." })
     }
 });
 
